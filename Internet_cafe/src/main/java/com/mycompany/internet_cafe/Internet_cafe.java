6bbf51d4d6db8f9bcd8c04f51a22f9166a42ac03
Wtlm <chunganh1312@gmail.com>
2024-04-26 10:55:04 +0700

    create project

---
+++ Internet_cafe/src/main/java/com/mycompany/internet_cafe/Internet_cafe.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.internet_cafe;
+
+/**
+ *
+ * @author Chung Anh
+ */
+public class Internet_cafe {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
